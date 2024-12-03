4aade7ca3396b7cbcf3be69a9d5392b4e095ecaf
Kristina047 <ch.drobot@gmail.com>
2024-12-03 16:13:54 +0300

    дз массив 1

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,42 @@ public class Main {
         String[] seasons  = {"Winter", "Spring", "Summer", "Autumn"};
 
         System.out.println("Задача 2");
-        int[] arr2 = new int[]{1,2,3};
-        arr2 [0] = 1;
-        arr2 [1] = 2;
-        arr2 [2] = 3;
+        int[] arr2 = {1,2,3};
         for (int i = 0; i < arr2.length ; i++) {
-            System.out.print(arr2[i]+",");
-        } System.out.println();
+            if (i == arr2.length - 1) {
+                System.out.println(arr2[i]);
+                break;
+            }
+            System.out.print(arr2[i] + ",");
+        }
 
         double[] arr3 = {1.57, 7.654, 9.986};
-        arr3 [0] = 1.57;
-        arr3 [1] = 7.654;
-        arr3 [2] = 9.986;
         for (int i = 0; i < arr3.length ; i++) {
-            System.out.print(arr3[i]+",");
+            if (i == arr3.length - 1) {
+                System.out.println(arr3[i]);
+                break;
+            }
+            System.out.print(arr3[i] + ",");
         }
-        System.out.println();
+
 
         String[] seasons1  = {"Winter", "Spring", "Summer", "Autumn"};
-        seasons1 [0] = "Winter";
-        seasons1 [1] = "Spring";
-        seasons1 [2] = "Summer";
-        seasons1 [3] = "Autumn";
         for (int i = 0; i < seasons1.length ; i++) {
-            System.out.print(seasons1[i]+",");
+            if (i == seasons1.length - 1) {
+                System.out.println(seasons1[i]);
+                break;
+            }
+            System.out.print(seasons1[i] + ",");
         }
+
+
+
+        System.out.println("Задача 3");
+        int[] arr4 = {1,2,3};
+        for (int i = 0; i < arr4.length; i--) {
+
+        }
+
 
     }
 }
